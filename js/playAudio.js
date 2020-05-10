2ac447cba69f569ac8ec19e48984ec7cb83e4bd9
ecat <qule520@126.com>
2020-05-10 09:38:29 +0800

    移除pycmd

--- js/playAudio.js
+++ js/playAudio.js
@@ -1,18 +1,11 @@
 //播放音频函数，需要传入音频地址
-function playAudio(audioSrc){
+function playAudio(audioSrc) {
 	// alert(audioSrc)
 	//尝试获取语速
-	var spd=document.getElementById('spd')
- 	// console.log(spd)
- 	spd=spd?spd.value:'3'
- 	audioSrc+="&spd="+spd
- 	//判断是否为PC端，PC和移动端的播放方式不一样
- 	if(typeof(pycmd)=="function"){
- 		//PC端，需要代码为1265623513的插件依赖
- 		pycmd("urlplay"+audioSrc)
-	}else{
- 		//移动端
- 		var player=new Audio(audioSrc)
- 		player.play()
- 	}
+	var spd = document.getElementById('spd')
+	// console.log(spd)
+	spd = spd ? spd.value : '3'
+	audioSrc += "&spd=" + spd
+	var player = new Audio(audioSrc)
+	player.play()
 }
